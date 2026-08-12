b659b44173bd7d1a2b0ccdecb4af8bd4e9664404
valen <124078971+ValentinPP2001@users.noreply.github.com>
2026-08-12 09:45:11 -0300

    Update admin.py

--- comprobantes/admin.py
+++ comprobantes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from comprobantes.models import TipoComprobante, Comprobante
 
-# Register your models here.
+@admin.register(TipoComprobante)
+class TipoComprobanteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nombre', 'descripcion']
+    list_editable = ['nombre']
+    search_fields = ['nombre']
+    list_per_page = 15
+    
+@admin.register(Comprobante)
+class ComprobanteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'fecha_hora_emision', 'tipo_comprobante', 'gasto_no_programado', 'observaciones', 'archivo']
+    list_editable = ['fecha_hora_emision', 'tipo_comprobante']
+    search_fields = ['fecha_hora_emision', 'tipo_comprobante']
+    list_per_page = 15
